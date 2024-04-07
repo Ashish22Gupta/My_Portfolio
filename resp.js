96a1fbf558d63e4f62e64546261bf54f90856bfb
Gupta Ashish Maharanidin <154006215+Ashish22Gupta@users.noreply.github.com>
2024-04-07 20:37:02 +0530

    Update resp.js

--- resp.js
+++ resp.js
@@ -1,9 +1,15 @@
-burger =document.querySelector('.burger')
+burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.nav-list')
 rightnav = document.querySelector('.rightnav')
 
-burger.addEventListener('click', ()=>{
+burger.addEventListener('click', () => {
+    rightnav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
+// Add this event listener to close the burger menu when a menu item is clicked
+navList.addEventListener('click', () => {
     rightnav.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
